8ac216c0909d490dea93f294b5320b90278d1be5
Jakerty <jeykoflorentino@gmail.com>
2023-05-20 15:24:40 -0400

    Cambios

--- calculator.c
+++ calculator.c
@@ -3,10 +3,12 @@
 int main() {
     int num1, num2, sum;
 
-    printf("Enter the first number: \n");
+    printf("Enter the first number: ");
+    fflush(stdout);
     scanf("%d", &num1);
 
-    printf("Enter the second number: \n");
+    printf("Enter the second number: ");
+    fflush(stdout);
     scanf("%d", &num2);
 
     sum = num1 + num2;
